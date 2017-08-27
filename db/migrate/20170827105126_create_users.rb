35eb41b69831f8c980160cc64b915380084b18f8
Eiji Hachiya <hachiyae@gmail.com>
2017-08-27 20:04:49 +0900

    add school_id

--- db/migrate/20170827105126_create_users.rb
+++ db/migrate/20170827105126_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :login_id
       t.string :password_digest
+      t.integer :school_id
       t.timestamps
     end
   end
